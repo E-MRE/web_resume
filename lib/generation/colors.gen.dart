239b02edbe0dd90f78f2cb4c560bfa6465803ece
Emre Gültekir <gltkr.emre@gmail.com>
2023-01-18 00:14:41 +0300

    fonts, colors & scripts added.

---
+++ lib/generation/colors.gen.dart
@@ -0,0 +1,39 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #DE5246
+  static const Color error = Color(0xFFDE5246);
+
+  /// Color: #FF7E73
+  static const Color errorLight = Color(0xFFFF7E73);
+
+  /// Color: #1A0000
+  static const Color primary = Color(0xFF1A0000);
+
+  /// Color: #58287F
+  static const Color primaryLight = Color(0xFF58287F);
+
+  /// Color: #89C4E1
+  static const Color secondary = Color(0xFF89C4E1);
+
+  /// Color: #F5EDCE
+  static const Color secondaryLight = Color(0xFFF5EDCE);
+
+  /// Color: #00C16F
+  static const Color success = Color(0xFF00C16F);
+
+  /// Color: #2FDF94
+  static const Color successLight = Color(0xFF2FDF94);
+}
